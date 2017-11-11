423479c64cc4ae9cfb50d147c8df9548dd442d29
Max <xamskier@aol.com>
2017-11-11 00:34:58 -0500

    functioning flight and pause with png's

--- chopper.js
+++ chopper.js
@@ -4,9 +4,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // Variable this may change during gameplay
   let gameState = "play";
-  let chopper;
-  var chopperImage = new Image();
-  chopperImage.src = "images/helicopter-spritesheet.png";
+  var chopper = new Image ();
+  chopper.src = "images/Superman-PNG-Pic.png";
+  let chopperWidth = 150;
+  let chopperHeight = 250;
+  let chopperXPos;
+  let chopperYPos;
+  // var chopperImage = new Image();
+  // chopperImage.src = "images/helicopter-spritesheet.png";
   let score;
   var flying = false;
   let startFlyRate = 4;
@@ -17,78 +22,81 @@ document.addEventListener("DOMContentLoaded", () => {
   let gravity = 1.5;
   let termVel = 10;
 
-  	function gameLoop () {
-
-  	  window.requestAnimationFrame(gameLoop);
-
-  	  chopper.update();
-  	  chopper.render();
-  	}
-
-  	function sprite (options) {
-
-  		var obj = {},
-  			frameIndex = 0,
-  			tickCount = 0,
-  			ticksPerFrame = options.ticksPerFrame || 0,
-  			numberOfFrames = options.numberOfFrames || 1;
-
-  		sprite.context = options.context;
-  		sprite.width = options.width;
-  		sprite.height = options.height;
-  		sprite.image = options.image;
-
-  		sprite.update = function () {
-
-              tickCount += 1;
-
-              if (tickCount > ticksPerFrame) {
-
-  				tickCount = 0;
-
-                  // If the current frame index is in range
-                  if (frameIndex < numberOfFrames - 1) {
-                      // Go to the next frame
-                      frameIndex += 1;
-                  } else {
-                      frameIndex = 0;
-                  }
-              }
-          };
-
-  		sprite.render = function () {
-
-  		  // Clear the canvas
-  		  ctx.clearRect(0, 0, sprite.width, sprite.height);
-
-  		  // Draw the animation
-  		  sprite.ctx.drawImage(
-  		    sprite.image,
-  		    frameIndex * sprite.width / numberOfFrames,
-  		    0,
-  		    sprite.width / numberOfFrames,
-  		    sprite.height,
-  		    0,
-  		    0,
-  		    sprite.width / numberOfFrames,
-  		    sprite.height);
-  		};
-
-  		return sprite;
-  	}
-
-chopper = sprite({
-  context: canvas.getContext("2d"),
-  width: 200,
-  height: 100,
-  image: chopperImage,
-  numberOfFrames: 4,
-  ticksPerFrame: 4
-});
+//   	function gameLoop () {
+//
+//   	  window.requestAnimationFrame(gameLoop);
+//
+//   	  chopper.update();
+//   	  chopper.render();
+//   	}
+//
+//   	function sprite (options) {
+//
+//   		var obj = {},
+//   			frameIndex = 0,
+//   			tickCount = 0,
+//   			ticksPerFrame = options.ticksPerFrame || 0,
+//   			numberOfFrames = options.numberOfFrames || 1;
+//
+//   		sprite.context = options.context;
+//   		sprite.width = options.width;
+//   		sprite.height = options.height;
+//   		sprite.image = options.image;
+//
+//   		sprite.update = function () {
+//
+//               tickCount += 1;
+//
+//               if (tickCount > ticksPerFrame) {
+//
+//   				tickCount = 0;
+//
+//                   // If the current frame index is in range
+//                   if (frameIndex < numberOfFrames - 1) {
+//                       // Go to the next frame
+//                       frameIndex += 1;
+//                   } else {
+//                       frameIndex = 0;
+//                   }
+//               }
+//           };
+//
+//   		sprite.render = function () {
+//
+//   		  // Clear the canvas
+//   		  ctx.clearRect(0, 0, sprite.width, sprite.height);
+//
+//   		  // Draw the animation
+//   		  sprite.ctx.drawImage(
+//   		    sprite.image,
+//   		    frameIndex * sprite.width / numberOfFrames,
+//   		    0,
+//   		    sprite.width / numberOfFrames,
+//   		    sprite.height,
+//   		    0,
+//   		    0,
+//   		    sprite.width / numberOfFrames,
+//   		    sprite.height);
+//   		};
+//
+//   		return sprite;
+//   	}
+//
+// chopper = sprite({
+//   context: canvas.getContext("2d"),
+//   width: 200,
+//   height: 100,
+//   image: chopperImage,
+//   numberOfFrames: 4,
+//   ticksPerFrame: 4
+// });
 
   function setup() {
     // gameState = "pause";
     score = 0;
+    chopperXPos = 400;
+    chopperYPos = 300;
+    ctx.drawImage(chopper, chopperXPos, chopperYPos, chopperWidth, chopperHeight);
   }
 
   window.onload = function() {
@@ -122,81 +130,67 @@ chopper = sprite({
         pause();
       }
     }
-    // event.preventDefault();
+    event.preventDefault();
   });
+
 
   function gameStart() {
     console.log("i am game start");
     // debugger
     if (gameState === "play") {
-      chopper = new Chopper(200, 150, 30, 'red');
+      // chopper = new Chopper(100, 200);
       ctx.clearRect(0, 0, canvas.width, canvas.height);
-      chopper.drawChopper();
-      chopper.fly();
+      drawChopper();
+      fly();
     }
   }
 
-  function Chopper(x, y, radius, color) {
-    this.x = x;
-    this.y = y;
-    this.radius = radius;
-    this.color = color;
+    // this.x = x;
+    // this.y = y;
+    // this.radius = radius;
+    // this.color = color;
 
-    this.drawChopper = function () {
+    function drawChopper () {
       console.log("i am drawChopper");
       // debugger
       if (gameState === "play") {
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
-        ctx.fillStyle = this.color;
-        ctx.fill();
-        ctx.closePath();
+        ctx.drawImage(chopper, chopperXPos, chopperYPos, chopperWidth, chopperHeight);
+        // ctx.beginPath();
+        // ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
+        // ctx.fillStyle = this.color;
+        // ctx.fill();
+        // ctx.closePath();
       }
-    };
+    }
 
-    this.fly = function() {
+    function fly () {
       // debugger
       console.log("i am fly");
       if(flying) {
         flyRate = startFlyRate;
-        this.y = this.y - flyRate;
+        chopperYPos = chopperYPos - flyRate;
 
         if(flyRate < termVel) {
             flyRate += lift;
         }
     } else {
         descRate = startDescRate;
-        this.y = this.y + descRate;
+        chopperYPos = chopperYPos + descRate;
 
         if(!(descRate > termVel)) {
           descRate += gravity;
         }
     }
     // this.drawChopper();
-  };
+  }
 
-  this.update = function() {
-    this.fly();
-  };
+  function update () {
+    fly();
+  }
 
-  this.render = function() {
+  function render () {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    this.drawChopper();
-  };
-
-    // this.update = function() {
-    //   if (this.y + this.radius > canvas.height) {
-    //     this.descRate = 0;
-    //     this.y = canvas.height - 30;
-    //     pause();
-    //     console.log(gameState);
-    //     // this.descRate = -this.descRate;
-    //   } else {
-    //     this.descRate += 1;
-    //   }
-    //   this.y += this.descRate;
-    // };
-
+    drawChopper();
   }
 
   document.addEventListener('keydown', (event) => {
@@ -233,8 +227,8 @@ chopper = sprite({
     if (elapsedTime > FPSInterval) {
       startTime = endTime - (elapsedTime - FPSInterval);
       if (gameState !== "pause") {
-        chopper.update();
-        chopper.render();
+        update();
+        render();
       }
     }
   }
@@ -244,6 +238,18 @@ chopper = sprite({
 });
 
 
+// this.update = function() {
+//   if (this.y + this.radius > canvas.height) {
+//     this.descRate = 0;
+//     this.y = canvas.height - 30;
+//     pause();
+//     console.log(gameState);
+//     // this.descRate = -this.descRate;
+//   } else {
+//     this.descRate += 1;
+//   }
+//   this.y += this.descRate;
+// };
 
 
 // sdsfds
