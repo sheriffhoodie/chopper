c729ef51681e5412bced50f6bf5a52a7d7c2bbb2
Max <xamskier@aol.com>
2017-11-13 01:13:46 -0500

    code clean up, added restart function upon crashing

--- chopper.js
+++ chopper.js
@@ -2,7 +2,6 @@ document.addEventListener("DOMContentLoaded", () => {
   const canvas = document.querySelector("canvas");
   const ctx = canvas.getContext('2d');
 
-  // Variables that may change during gameplay
   let gameState;
 
   var chopper = new Image ();
@@ -19,9 +18,10 @@ document.addEventListener("DOMContentLoaded", () => {
   var bgVelocity = 30;
   var background = new Image ();
   background.src = "images/space-bkgd.jpg";
-  let scrollSpeed;
+  let scrollX;
 
   let score;
+  let borderCrash;
   var flying = false;
   let startFlyRate = 4;
   let startDescRate = 6;
@@ -31,94 +31,28 @@ document.addEventListener("DOMContentLoaded", () => {
   let gravity = 1.5;
   let termVel = 10;
 
-//   	function gameLoop () {
-//
-//   	  window.requestAnimationFrame(gameLoop);
-//
-//   	  chopper.update();
-//   	  chopper.render();
-//   	}
-//
-//   	function sprite (options) {
-//
-//   		var obj = {},
-//   			frameIndex = 0,
-//   			tickCount = 0,
-//   			ticksPerFrame = options.ticksPerFrame || 0,
-//   			numberOfFrames = options.numberOfFrames || 1;
-//
-//   		sprite.context = options.context;
-//   		sprite.width = options.width;
-//   		sprite.height = options.height;
-//   		sprite.image = options.image;
-//
-//   		sprite.update = function () {
-//
-//               tickCount += 1;
-//
-//               if (tickCount > ticksPerFrame) {
-//
-//   				tickCount = 0;
-//
-//                   // If the current frame index is in range
-//                   if (frameIndex < numberOfFrames - 1) {
-//                       // Go to the next frame
-//                       frameIndex += 1;
-//                   } else {
-//                       frameIndex = 0;
-//                   }
-//               }
-//           };
-//
-//   		sprite.render = function () {
-//
-//   		  // Clear the canvas
-//   		  ctx.clearRect(0, 0, sprite.width, sprite.height);
-//
-//   		  // Draw the animation
-//   		  sprite.ctx.drawImage(
-//   		    sprite.image,
-//   		    frameIndex * sprite.width / numberOfFrames,
-//   		    0,
-//   		    sprite.width / numberOfFrames,
-//   		    sprite.height,
-//   		    0,
-//   		    0,
-//   		    sprite.width / numberOfFrames,
-//   		    sprite.height);
-//   		};
-//
-//   		return sprite;
-//   	}
-//
-// chopper = sprite({
-//   context: canvas.getContext("2d"),
-//   width: 200,
-//   height: 100,
-//   image: chopperImage,
-//   numberOfFrames: 4,
-//   ticksPerFrame: 4
-// });
-
   function setup() {
-    // gameState = "pause";
     score = 0;
     chopperXPos = 75;
     chopperYPos = 100;
-    scrollSpeed = 0;
+    scrollX = 0;
+    borderCrash = 0;
     ctx.drawImage(chopper, chopperXPos, chopperYPos, chopperWidth, chopperHeight);
     ctx.drawImage(background, 0, 0, bgWidth, bgHeight);
-    ctx.fillStyle = "rgb(255,255,255)";
-    ctx.fillText('Press spacebar to play/pause', 350, 600);
   }
 
   window.onload = function() {
   setup();
 };
 
+  // Game State Modes
+  function clear() {
+    console.log("i am clearing");
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  }
+
   function play() {
     if (gameState === "pause") {
-      // window.requestAnimationFrame(gameStart, canvas);
       gameState = "play";
     }
   }
@@ -129,9 +63,12 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
 
-  function stop() {
-    gameState = "stop";
+  function restart() {
+    clear();
+    setup();
+    gameStart();
   }
+
 
   //play/pause with spacebar
   document.addEventListener("keypress", (event) => {
@@ -146,71 +83,83 @@ document.addEventListener("DOMContentLoaded", () => {
     event.preventDefault();
   });
 
+  document.addEventListener("keypress", (event) => {
+    console.log("i am restarting");
+    if (event.keyCode === 82) {
+      restart();
+    }
+    // event.preventDefault();
+  });
+
 
   function gameStart() {
-    // console.log("i am game start");
-    // debugger
-      // chopper = new Chopper(100, 200);
-      gameState = "pause";
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      drawBackground();
-      drawChopper();
-      fly();
-      ctx.font = "18 helvetica";
-      ctx.fillStyle = "white";
-      ctx.fillText('Press spacebar to play/pause', 350, 600);
+  // console.log("i am game start");
+    gameState = "pause";
+    clear();
+    drawBackground();
+    drawChopper();
+    fly();
   }
 
-    function drawBackground () {
-      console.log("i am drawing the bg");
-      if (scrollSpeed >= canvas.width) {
-        scrollSpeed = 0;
-      }
-      scrollSpeed += bgVelocity;
-      ctx.drawImage(background, -scrollSpeed, 0, bgWidth, bgHeight);
-      ctx.drawImage(background, canvas.width - scrollSpeed, 0, bgWidth, bgHeight);
+  function drawBackground () {
+    // console.log("i am drawing the bg");
+    if (scrollX >= canvas.width) {
+      scrollX = 0;
     }
+    scrollX += bgVelocity;
+    ctx.drawImage(background, -scrollX, 0, bgWidth, bgHeight);
+    ctx.drawImage(background, canvas.width - scrollX, 0, bgWidth, bgHeight);
+  }
 
-    function drawChopper () {
-      // console.log("i am drawChopper");
-      // debugger
-      if (gameState === "play") {
-        ctx.drawImage(chopper, chopperXPos, chopperYPos, chopperWidth, chopperHeight);
-        // ctx.beginPath();
-        // ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
-        // ctx.fillStyle = this.color;
-        // ctx.fill();
-        // ctx.closePath();
-      }
+  function drawChopper () {
+    // console.log("i am drawChopper");
+    if (gameState === "play") {
+      ctx.drawImage(chopper, chopperXPos, chopperYPos, chopperWidth, chopperHeight);
     }
+  }
 
-    function fly () {
-      // debugger
-      // console.log("i am fly");
-      if(flying) {
-        flyRate = startFlyRate;
-        chopperYPos = chopperYPos - flyRate;
-
-        if(flyRate < termVel) {
-            flyRate += lift;
-        }
+  function borderCrashCheck() {
+    if (flying) {
+      if (chopperYPos <= 0) {
+        borderCrash = true;
+        gameState = "pause";
+        restart();
+      }
     } else {
-        descRate = startDescRate;
-        chopperYPos = chopperYPos + descRate;
-
-        if(!(descRate > termVel)) {
-          descRate += gravity;
-        }
+      if (chopperYPos + chopperHeight > canvas.height) {
+        borderCrash = true;
+        gameState = "pause";
+        restart();
+      }
     }
-    // this.drawChopper();
+  }
+
+  function fly () {
+    // console.log("i am fly");
+    if(flying) {
+      flyRate = startFlyRate;
+      chopperYPos = chopperYPos - flyRate;
+
+      if(flyRate < termVel) {
+          flyRate += lift;
+      }
+    } else {
+      descRate = startDescRate;
+      chopperYPos = chopperYPos + descRate;
+
+      if(!(descRate > termVel)) {
+        descRate += gravity;
+      }
+    }
   }
 
   function update () {
+    borderCrashCheck();
     fly();
   }
 
   function render () {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    clear();
     drawBackground();
     drawChopper();
   }
@@ -258,20 +207,3 @@ document.addEventListener("DOMContentLoaded", () => {
   startAnimation(30);
 
 });
-
-
-// this.update = function() {
-//   if (this.y + this.radius > canvas.height) {
-//     this.descRate = 0;
-//     this.y = canvas.height - 30;
-//     pause();
-//     console.log(gameState);
-//     // this.descRate = -this.descRate;
-//   } else {
-//     this.descRate += 1;
-//   }
-//   this.y += this.descRate;
-// };
-
-
-// sdsfds
