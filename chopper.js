a7fa4d747f8ead14c77cc46d8a47c10130310882
Max <xamskier@aol.com>
2017-11-21 23:28:22 -0500

    centered intro info

--- chopper.js
+++ chopper.js
@@ -157,18 +157,18 @@ document.addEventListener("DOMContentLoaded", () => {
   function showIntro () {
     ctx.font = '36px Orbitron';
     ctx.fillStyle = 'gray';
-    ctx.fillText('Welcome to Space Chopper', 333, 100);
+    ctx.fillText('Welcome to Space Chopper', 313, 100);
     ctx.font = '22px Orbitron';
     ctx.fillStyle = 'orange';
-    ctx.fillText('Pilot the chopper as far into the asteroid field as you can!', 270, 160);
+    ctx.fillText('Pilot the chopper as far into the asteroid field as you can!', 250, 160);
     ctx.font = '22px Orbitron';
     ctx.fillStyle = 'orange';
-    ctx.fillText('Avoid asteroids and the top and bottom of the frame!', 292, 190);
+    ctx.fillText('Avoid asteroids and the top and bottom of the frame!', 272, 190);
     ctx.font = '30px Orbitron';
     ctx.fillStyle = 'white';
-    ctx.fillText('Press space bar to start', 395, 250);
+    ctx.fillText('Press space bar to start', 375, 250);
     ctx.font = '30px Orbitron';
-    ctx.fillText('Use "F" key to fly', 455, 295);
+    ctx.fillText('Use "F" key to fly', 435, 295);
   }
 
   //play/pause with spacebar
